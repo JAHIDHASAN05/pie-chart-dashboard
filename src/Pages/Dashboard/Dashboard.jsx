a8b2aafc7665740f3863971826dca47c266e13f9
JAHID HASAN <developerjahid@gmail.com>
2024-02-27 15:44:42 +0600

    initial structure done

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <section className='container d-flex '>
+
+            <div className='col-6'>
+               
+            </div>
+
+            <div className='col-6'>f </div>
+        </section>
+    );
+};
+
+export default Dashboard;
